fe8a2907a44c424c9f346dc522afc29edf1a4768
Levin Keller <levin@coyno.com>
2015-11-24 12:40:22 +0100

    Fix #21.

--- index.js
+++ index.js
@@ -7,9 +7,8 @@ bitcore.version = 'v' + require('./package.json').version;
 bitcore.versionGuard = function(version) {
   if (version !== undefined) {
     var message = 'More than one instance of bitcore-lib found. ' + 
-      'Please make sure to require bitcore-lib and check that submodules do' +
-      ' not also include their own bitcore-lib dependency.';
-    throw new Error(message);
+      'Please make sure that you are not mixing instances of classes of the different versions of bitcore.';
+    console.warn(message);
   }
 };
 bitcore.versionGuard(global._bitcore);
