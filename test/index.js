c8c12c2cd2aa1fccd4bcfc46766bc15a18d955fe
Stilldabomb <Stilldabomb@users.noreply.github.com>
2018-03-25 02:34:57 -0700

    Remove unneeded test
    Wanted to replace with something more useful, but would require a way to double import bitcore-lib.

--- test/index.js
+++ test/index.js
@@ -7,10 +7,4 @@ describe('#versionGuard', function() {
   it('global._bitcore should be defined', function() {
     should.equal(global._bitcore, bitcore.version);
   });
-
-  it('throw an error if version is already defined', function() {
-    (function() {
-      bitcore.versionGuard('version');
-    }).should.throw('More than one instance of bitcore');
-  });
 });
