98ffd925b0d981493653f7e8588cc9d71957cf2c
Alex Werner <alex@werner.fr>
2018-04-01 22:01:42 +0700

    Impr : Will now generate both uglified and standard file.

--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,33 @@ const path = require('path');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 const commonJSConfig = {
-  entry: ['./index.js'],
   module: {
-    rules: [
-    ],
+    rules: [],
   },
+
+
+};
+
+const rawConfig = Object.assign({}, commonJSConfig, {
+  entry: ['./index.js'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bitcore-lib-dash.js',
+    library: 'bitcore-lib-dash',
+    libraryTarget: 'umd',
+  }
+})
+const uglifiedConfig = Object.assign({}, commonJSConfig, {
+  entry: ['./dist/bitcore-lib-dash.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bitcore-lib-dash.min.js',
     library: 'bitcore-lib-dash',
     libraryTarget: 'umd',
   },
   plugins: [
     new UglifyJsPlugin()
   ]
+})
 
-};
-
-module.exports = [commonJSConfig];
+module.exports = [rawConfig, uglifiedConfig];
