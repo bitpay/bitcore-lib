e5569616465d096af6125663723b8bd73cd5d325
Alex Werner <alex@werner.fr>
2018-05-08 16:52:33 +0700

    Doc : Dashified error

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ var bitcore = module.exports;
 bitcore.version = 'v' + require('./package.json').version;
 bitcore.versionGuard = function(version) {
   if (version !== undefined) {
-    var message = 'More than one instance of bitcore-lib found. ' + 
-      'Please make sure that you are not mixing instances of classes of the different versions of bitcore.';
+    var message = 'More than one instance of dashcore-lib found. ' +
+      'Please make sure that you are not mixing instances of classes of the different versions of dashcore.';
     console.warn(message);
   }
 };
