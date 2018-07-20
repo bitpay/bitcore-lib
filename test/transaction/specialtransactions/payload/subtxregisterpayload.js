cc85ce211d61eb556e87440c4c62aea933d42dd1
Anton Suprunchuk <antouhou@gmail.com>
2018-07-20 13:29:45 +0700

    Add test framework for SubTxRegisterPayload

--- test/transaction/specialtransactions/payload/subtxregisterpayload.js
+++ test/transaction/specialtransactions/payload/subtxregisterpayload.js
@@ -7,5 +7,48 @@ var Payload = SpecialTransactions.payload;
 var SubTxRegisterPayload = Payload.SubTxRegisterPayload;
 
 describe('SubTxRegisterPayload', function() {
-
+  describe('#constructor', function () {
+    it('Should create SubTxRegisterPayload instance', function () {
+      var payload = new SubTxRegisterPayload();
+      expect(payload).to.have.property('nVersion');
+    });
+  });
+  describe('parsePayloadBuffer', function () {
+    throw new Error('Not implemented');
+  });
+  describe('parsePayloadJSON', function () {
+    throw new Error('Not implemented');
+  });
+  describe('#setUserName', function () {
+    throw new Error('Not implemented');
+  });
+  describe('#setPubKeyId', function () {
+    throw new Error('Not implemented');
+  });
+  describe('#setPubKeyIdFromPrivateKey', function () {
+    throw new Error('Not implemented');
+  });
+  describe('#sign', function () {
+    throw new Error('Not implemented');
+  });
+  describe('#verifySignature', function () {
+    throw new Error('Not implemented');
+  });
+  describe('#toJSON', function () {
+    throw new Error('Not implemented');
+  });
+  describe('#toBuffer', function () {
+    throw new Error('Not implemented');
+  });
+  describe('#getHash', function() {
+    it('Should return hash', function () {
+      throw new Error('Not implemented');
+    });
+    it('Should return hash without signature if option passed', function () {
+      throw new Error('Not implemented');
+    });
+    it('Should throw if data is incomplete', function () {
+      throw new Error('Not implemented');
+    });
+  });
 });
