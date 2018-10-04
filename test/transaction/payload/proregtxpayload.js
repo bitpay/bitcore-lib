35cc49ef295df078210cc21ae86880237b953f94
Pierre <pierre@dash.org>
2018-10-04 12:00:54 +0200

    first working test

--- test/transaction/payload/proregtxpayload.js
+++ test/transaction/payload/proregtxpayload.js
@@ -2,11 +2,9 @@ var expect = require('chai').expect;
 
 var DashcoreLib = require('../../..');
 
-var PrivateKey = DashcoreLib.PrivateKey;
 var BufferUtil = DashcoreLib.util.buffer;
 var Payload = DashcoreLib.Transaction.Payload;
 var ProRegTxPayload = Payload.ProRegTxPayload;
-var isHexString = DashcoreLib.util.js.isHexaString;
 
 describe('ProRegTxPayload', function () {
 
@@ -16,33 +14,32 @@ describe('ProRegTxPayload', function () {
       expect(payload).to.have.property('version');
     });
   });
+
   describe('fromBuffer', function () {
     it('Should return instance of ProRegTxPayload with parsed data', function () {
 
       var options = {
         type: 1,
         mode: 2,
-        collateralIndex: 3,
-        ipAddress: '100.225.225.1',
+        collateralIndex: 1,
+        ipAddress: '0000:0000:0000:0000:0000:ffff:c0a8:0001', //192.168.0.1
         port: 10000,
-        KeyIdOwner: 'aaa143e6cf3b975d0376',
-        KeyIdOperator: 'bbb143e6cf3b975d0376',
-        KeyIdVoting: 'ccc143e6cf3b975d0376',
+        KeyIdOwner: '4d5fce2325deb034ae75a625a3e2f09395e27bf7',
+        KeyIdOperator: '4d5fce2325deb034ae75a625a3e2f09395e27bf7',
+        KeyIdVoting: '4d5fce2325deb034ae75a625a3e2f09395e27bf7',
         operatorReward: 100,
-        scriptPayoutSize: 1234,
-        scriptPayout: 101,
+        scriptPayout: 'deb034ae',
         inputsHash: 'a6f7b4284fb753eab9b554283c4fe1f1d7e143e6cf3b975d0376d7c08ba4cdf5',
-        payloadSigSize: 1024,
         payloadSig: '48d6a1bd2cd9eec54eb866fc71209418a950402b5d7e52363bfb75c98e141175',
       }
 
       var payloadBuffer = new ProRegTxPayload(options).toBuffer();
-
       expect(BufferUtil.isBuffer(payloadBuffer)).to.be.true;
 
       var parsedPayload = ProRegTxPayload.fromBuffer(payloadBuffer);
-      expect(parsedPayload.userName).to.be.equal('test');
-      expect(BufferUtil.equals(parsedPayload.pubKeyId, pubKeyId)).to.be.true;
+      for (var key in options) {
+        expect(parsedPayload[key]).to.be.equal(options[key]);
+      }
 
     });
 
